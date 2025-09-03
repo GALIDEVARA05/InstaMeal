587df98cc1f64480ff9247eba12f28cca470e0e8
Galidevara Venkata Lokesh <galidevaravenkatalokesh05@gmail.com>
2025-09-03 14:44:41 +0530

    url name changed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 // src/api/axios.js
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const baseURL = process.env.VITE_API_URL || "http://localhost:5000/api";
 
 console.log("🔗 Axios BaseURL:", baseURL);
 
